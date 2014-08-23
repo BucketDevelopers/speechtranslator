c88792ca205837b59f146680de204b385a993719
Aparna H S <hs.aparna.blr@gmail.com>
2014-08-23 21:54:23 +0530

    Text to text translation

---
+++ Interpreter/src/com/bucketdevelopers/Interpreter/TransClass.java
@@ -0,0 +1,23 @@
+package com.bucketdevelopers.Interpreter;
+
+import com.memetix.mst.language.Language;
+import com.memetix.mst.translate.Translate;
+
+public class TransClass {
+	
+	public static void translate(String text,String lang1,String lang2) throws Exception
+	{
+		//Replace client_id and client_secret with your own.  
+	    Translate.setClientId("speechtrans1");
+	    Translate.setClientSecret("ZGc8q56kWfuSEJplym/uaAT2+v1USgok/5kMwI19oks=");
+	 
+	    // Translate text from lang1 to lang2
+	    Language l1=Language.valueOf(lang2);
+	    String textTranslated = Translate.execute(text,l1);
+	 
+	    System.out.println("Original langauge"+lang1+" phrase: " + text );
+	    System.out.println("Translated to language"+lang2+" translated phrase: " + textTranslated);
+	   
+	}
+
+}
